5f947aaf93415e14e2d108ad53810762e12d4017
moqsien <moqsien@foxmail.com>
2024-02-19 17:31:18 +0800

    autopairs

--- lua/gnvim/cmp.lua
+++ lua/gnvim/cmp.lua
@@ -74,3 +74,51 @@ cmp.setup.cmdline(':', {
         { name = 'cmdline' }
     })
 })
+
+-- Autopairs
+local npairs = require("nvim-autopairs")
+local Rule = require('nvim-autopairs.rule')
+
+npairs.setup({
+    check_ts = true,
+    ts_config = {
+        lua = {'string', 'source'},-- it will not add a pair on that treesitter node
+        javascript = {'string', 'template_string'},
+        java = false,-- don't check treesitter on java
+    },
+    disable_filetype = {'TelescopePrompt', 'spectre_panel'},
+    fast_wrap = {
+        map = '<M-e>',
+        chars = { '{', '[', '(', '"', "'" },
+        pattern = [=[[%'%"%>%]%)%}%,]]=],
+        end_key = '$',
+        before_key = 'h',
+        after_key = 'l',
+        cursor_pos_before = true,
+        keys = 'qwertyuiopzxcvbnmasdfghjkl',
+        manual_position = true,
+        highlight = 'Search',
+        highlight_grey='Comment'
+    },
+})
+
+local ts_conds = require('nvim-autopairs.ts-conds')
+
+
+-- press % => %% only while inside a comment or string
+npairs.add_rules({
+  Rule("%", "%", "lua")
+    :with_pair(ts_conds.is_ts_node({'string','comment'})),
+  Rule("$", "$", "lua")
+    :with_pair(ts_conds.is_not_ts_node({'function'}))
+})
+
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+cmp.event:on(
+  'confirm_done',
+  cmp_autopairs.on_confirm_done()
+)
+--
+
+-- surround
+require("nvim-surround").setup()
--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,21 +1,36 @@
 -- Completion: https://github.com/hrsh7th/nvim-cmp
+-- Autopairs: https://github.com/windwp/nvim-autopairs
+-- Surround: https://github.com/kylechui/nvim-surround
 
 return {
-  "hrsh7th/nvim-cmp",
-  dependencies = {
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-cmdline",
-      "saadparwaiz1/cmp_luasnip",
-      {
-          "saadparwaiz1/cmp_luasnip",
-          dependencies = {
-              "L3MON4D3/LuaSnip",
-              dependencies = {
-                  "rafamadriz/friendly-snippets",
-              }
-          }
-      },
-  },
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-cmdline",
+            "saadparwaiz1/cmp_luasnip",
+            {
+                "saadparwaiz1/cmp_luasnip",
+                dependencies = {
+                    "L3MON4D3/LuaSnip",
+                    dependencies = {
+                        "rafamadriz/friendly-snippets",
+                    }
+                }
+            },
+        },
+    },
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+    },
+    {
+        "kylechui/nvim-surround",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-treesitter/nvim-treesitter-textobjects",            
+        },
+    },
 }
