b5224ea8b758d25e4793e6126c96201ba09f62a1
moqsien <moqsien@foxmail.com>
2024-02-19 16:05:16 +0800

    blankline

--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -1,8 +1,14 @@
 -- Syntax Highlighting: https://github.com/nvim-treesitter/nvim-treesitter
 -- Rainbow Parentheses: https://github.com/HiPhish/rainbow-delimiters.nvim
+-- Blankline: https://github.com/lukas-reineke/indent-blankline.nvim
+
 return {
     {
         'HiPhish/rainbow-delimiters.nvim',
         dependencies = { 'nvim-treesitter/nvim-treesitter' }
+    },
+    { 
+        "lukas-reineke/indent-blankline.nvim", 
+        main = "ibl", 
     },
 }
